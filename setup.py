8bf8c338f1a0fc01fc9b58e1aebbfb6c678a086d
Leonardo Merza <ljmerza@gmail.com>
2019-07-17 14:34:40 -0400

    automated commit 17/07/2019 14:34:40

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+
+PACKAGES = find_packages(exclude=['tests', 'tests.*'])
+
+
 setup(
     name='py-our-groceries',  
-    version='0.16',
+    version='0.17',
     author="Leonardo Merza",
     author_email="ljmerza@gmail.com",
     keywords='unoffical our groceries api',
@@ -11,7 +15,10 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ljmerza/py-our-groceries",
     license='MIT',
-    packages=find_packages(),
+    packages=PACKAGES,
+    include_package_data=True,
+    python_requires='>=3',
+    zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
