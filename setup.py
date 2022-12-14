24cfa77be4d5ae159860b0ecd1bd741e2495709e
Leonardo Merza <ljmerza@gmail.com>
2022-12-14 09:46:52 -0500

    change deps

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ REQUIRES = [
 
 setup(
     name='ourgroceries',
-    version='1.5.0',
+    version='1.5.1',
     author="Leonardo Merza",
     author_email="ljmerza@gmail.com",
     keywords='unoffical our groceries api',
