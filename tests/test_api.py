69ada91933fe1e24c548b5801188d5b1dde105e2
Lawrence Kesteloot <lk@teamten.com>
2026-09-16 06:02:29 -0700

    Add support for bulk editing of items in a single list. (#27)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,7 @@
 
 
 import asyncio
-from ourgroceries import OurGroceries
+from ourgroceries import OurGroceries, make_delete_item_edit_record
 
 username = ''
 password = ''
@@ -12,5 +12,14 @@ asyncio.run(og.login())
 my_lists = asyncio.run(og.get_my_lists())
 print(my_lists)
 
-my_todo_list = asyncio.run(og.get_list_items(list_id=''))
+list_id = ''
+my_todo_list = asyncio.run(og.get_list_items(list_id=list_id))
 print(my_todo_list)
+
+# Delete all crossed-off (done) items.
+crossed_off_item_ids = [item["id"] for item in my_todo_list["list"]["items"] if "crossedOffAt" in item]
+print(crossed_off_item_ids)
+
+result = asyncio.run(og.edit_items(list_id, [make_delete_item_edit_record(item_id)
+                                             for item_id in crossed_off_item_ids]))
+print(result)
