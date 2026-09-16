f72594be42416253722647e002f030d053a16816
Leonardo Merza <ljmerza@gmail.com>
2026-09-16 09:04:34 -0400

    bump version to 1.6.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ REQUIRES = [
 
 setup(
     name='ourgroceries',
-    version='1.5.3',
+    version='1.6.0',
     author="Leonardo Merza",
     author_email="ljmerza@gmail.com",
     keywords='unoffical our groceries api',
